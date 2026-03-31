895b2fad2fd67c45940906bcb659c32ec7faec4f
M Fauzan Lubada <fauzanlubada5@gmail.com>
2026-03-31 12:55:04 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Force error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Fix temporary storage paths for Vercel
 $storagePath = '/tmp/storage';
 
